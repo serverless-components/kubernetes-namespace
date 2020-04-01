4985ad89b148ff91041987a946f63997da39ad70
Philipp Muens <raute1337@gmx.de>
2020-04-01 14:36:02 +0200

    Fix parsing of TLS verification skipping config

--- src/serverless.js
+++ src/serverless.js
@@ -49,7 +49,7 @@ class KubernetesNamespace extends Component {
   getKubernetesClient(type) {
     const { endpoint, port } = this.credentials.kubernetes
     const token = this.credentials.kubernetes.serviceAccountToken
-    const skipTLSVerify = this.credentials.kubernetes.skipTlsVerify ? true : false
+    const skipTLSVerify = this.credentials.kubernetes.skipTlsVerify == 'true'
     const kc = new kubernetes.KubeConfig()
     kc.loadFromOptions({
       clusters: [
